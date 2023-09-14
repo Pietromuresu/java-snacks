eaadede101845ba3cd39d8dccecde82f84321398
Pietromuresu <muresupietro66@gmail.com>
2023-09-14 16:28:23 +0200

    Snack 8 - completed

--- src/org/java/Snack8.java
+++ src/org/java/Snack8.java
@@ -14,15 +14,37 @@ public class Snack8 {
 		
 		int[] arrayNum = new int[10];
 		
+		int sum = 0;
+		int avg = 0;
+		
+		
 		for(int i = 0; i < arrayNum.length; i++) {
 			arrayNum[i] = rand.nextInt(100, 150);
 		}
+		
+		
+		int max = arrayNum[0];
+		int min = arrayNum[0];
 
+		
 		for(int i = 0; i < arrayNum.length; i++) {
+		
 			System.out.println(i + ". " + arrayNum[i]);
+			if(arrayNum[i] > max) {
+				max = arrayNum[i];
+			}
+			
+			if(arrayNum[i] < min) {
+				min = arrayNum[i];
+			}
 		}
 		
 		
+		avg /= arrayNum.length;
 		
+		
+		System.out.println("Average: " + avg);
+		System.out.println("Max value: " + max);
+		System.out.println("Min value: " + min);
 	}
 }
