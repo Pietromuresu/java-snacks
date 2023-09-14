d4f0ed0fea1e4b3b9e6f79098205ffa0d8bda507
Pietromuresu <muresupietro66@gmail.com>
2023-09-14 16:30:48 +0200

    Snack 9 - started

---
+++ src/org/java/Snack9.java
@@ -0,0 +1,16 @@
+package org.java;
+
+import java.util.Scanner;
+
+public class Snack9 {
+	public static void main(String[] args) {
+		
+		
+//		Chiedere all'utente numeri interi e calcolarene la somma. 
+//		Continuare a chiedere numeri, finche' la sommatoria non supera il valore `1000`
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+}
