1c92bcc0b3269be752ead5d4b8b3464fc738e281
Pietromuresu <muresupietro66@gmail.com>
2023-09-14 11:37:38 +0200

    Snack 1

--- src/org/java/Snack1.java
+++ src/org/java/Snack1.java
@@ -1,5 +1,25 @@
 package org.java;
 
+import java.util.Scanner;
+
 public class Snack1 {
 
+	
+	public static void main(String[] args) {
+		
+		// Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digit a number: ");
+		int selectedNumber = sc.nextInt();
+		
+		if(selectedNumber % 2 == 0) {
+			System.out.println("Il numero '" + selectedNumber + "' è pari");
+		}else {
+			selectedNumber++; 
+			System.out.println("Il numero '" + (selectedNumber - 1) + "' non è pari ma il successivo '" + selectedNumber + "' lo è");
+		}
+		
+	}
 }
